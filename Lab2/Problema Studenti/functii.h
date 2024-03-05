37f8a3d300bd64a8cb7a86e84ff26bda19155de2
MTAxD12 <andrei.foco@yahoo.com>
2024-03-06 00:32:32 +0200

    Lab 2

---
+++ Lab2/Problema Studenti/functii.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTII_H_INCLUDED
+#define FUNCTII_H_INCLUDED
+
+#include "student.h"
+
+int ComparaNume(Student student1, Student student2);
+int ComparaNotaMate(Student student1, Student student2);
+int ComparaNotaEng(Student student1, Student student2);
+int ComparaNotaIst(Student student1, Student student2);
+int ComparaMedie(Student student1, Student student2);
+
+#endif // FUNCTII_H_INCLUDED
+
---
+++ Lab2/Problema Studenti/student.h
@@ -0,0 +1,31 @@
+#ifndef STUDENT_H_INCLUDED
+#define STUDENT_H_INCLUDED
+
+#include <string.h>
+using namespace std;
+
+class Student
+{
+    char nume[100];
+    float notaMate;
+    float notaEngleza;
+    float notaIstorie;
+
+public:
+    Student();
+
+    void SetareNume(char* numeStudent);
+    char* GetNume();
+
+    void SetareNotaMate(float nota);
+    float GetNotaMate();
+
+    void SetareNotaEng(float nota);
+    float GetNotaEng();
+
+    void SetareNotaIst(float nota);
+    float GetNotaIst();
+
+    float GetMediaNot();
+};
+#endif // STUDENT_H_INCLUDED
